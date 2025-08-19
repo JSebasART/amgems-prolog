8f047a54941224c99ebf99dd8b99d52d7b4ada12
Juan Neftaly Castellanos Hernández <104848289+JuanNeftaly@users.noreply.github.com>
2025-08-19 16:36:24 -0600

    Add: prolog exercise 1

---
+++ query.pl
@@ -0,0 +1,11 @@
+:- [clase2]. % cargar la base de conocimiento
+
+
+% 1. que come el leon? 
+:- come(leon,que).
+
+% 2. que animales son comida del leon? 
+:- comida(X).	
+
+%3. la vaca es un mamifero?
+:- mamifero(vaca).
